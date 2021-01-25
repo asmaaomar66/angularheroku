44a9b2788e194bb1aceda9dad7265631d9f00221
AsmaaOmar <62079548+asmaaomar66@users.noreply.github.com>
2021-01-25 21:20:14 +0200

    zzzzzzzzzz

--- server.js
+++ server.js
@@ -1,16 +1,15 @@
-// //Install express server
-// const express = require('express');
-// const path = require('path');
+//Install express server
+const express = require('express');
+const path = require('path');
 
-// const app = express();
+const app = express();
 
-// // Serve only the static files form the angularapp directory
-// app.use(express.static(__dirname + '/angularapp'));
+// Serve only the static files form the dist directory
+app.use(express.static('./dist/angularheroku'));
 
-// app.get('/*', function(req,res) {
+app.get('/*', (req, res) =>
+    res.sendFile('index.html', {root: 'dist/angularheroku/'}),
+);
 
-// res.sendFile(path.join(__dirname+'/dist/index.html'));
-// });
-
-// // Start the app by listening on the default Heroku port
-// app.listen(process.env.PORT || 8080);
+// Start the app by listening on the default Heroku port
+app.listen(process.env.PORT || 8080);
