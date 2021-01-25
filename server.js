f069192c804048ce0c101ce3e3cdae565e723aba
AsmaaOmar <62079548+asmaaomar66@users.noreply.github.com>
2021-01-25 22:52:39 +0200

    -000-

--- server.js
+++ server.js
@@ -5,10 +5,10 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static('./dist/angularheroku'));
+app.use(express.static('./dist/AngularDynamicRoutes'));
 
 app.get('/*', (req, res) =>
-    res.sendFile('index.html', {root: 'dist/angularheroku/'}),
+    res.sendFile('index.html', {root: 'dist/AngularDynamicRoutes/'}),
 );
 
 // Start the app by listening on the default Heroku port
